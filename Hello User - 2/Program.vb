271f1a65793ed737fa274909b69f649c18b8c603
Ollie Foster <00.aegis-aseries@icloud.com>
2021-12-03 21:34:06 +0000

    initial commit

---
+++ Hello User - 2/Program.vb
@@ -0,0 +1,25 @@
+﻿'
+' Created by SharpDevelop.
+' User: 19ofoster
+' Date: 08/10/2021
+' Time: 10:58
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		'Dimesions a variabe as name
+		Dim name As String=""
+		
+		'Gets the user to input their name
+		Console.WriteLine("Please enter your name . . . ")
+		name = Console.ReadLine()
+		
+		'Outputs the answer to the user
+		Console.WriteLine("Hello, " & name)
+		
+		
+		Console.Write("Press any key to continue . . . ")
+		Console.ReadKey(True)
+	End Sub
+End Module
